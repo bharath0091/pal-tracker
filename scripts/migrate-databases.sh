3671f3ef6b3dc53aebcb95eef0b32f364b4aa657
ChennupatiBK <bharath.kumar.chennupati@rabobank.nl>
2019-07-25 14:45:34 +0200

    reinstated to original state for the db migration script

--- scripts/migrate-databases.sh
+++ scripts/migrate-databases.sh
@@ -5,8 +5,8 @@ set -e
 
 app_name="${1:-pal-tracker}"
 script_dir="${2:-.}"
-service_name="cleardb"
-service_key="sk"
+service_name="${3:-tracker-database}"
+service_key="${4:-flyway-migration-key}"
 
 function main() {
 
